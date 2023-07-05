3ff5540f961aa22b3288bc625d07eca37833d88e
Nghia-Hieu <63239928+Nghia-Hieu@users.noreply.github.com>
2023-07-05 10:11:37 +0700

    update offline notification

--- components/listeners/socket.js
+++ components/listeners/socket.js
@@ -1,4 +1,5 @@
 // exports = module.exports = function(io){
+import axios from "axios";
 export default function (io) {
   var map = [];
 
@@ -9,6 +10,7 @@ export default function (io) {
     };
 
     var room = null;
+    var username = "";
 
     var current_student_permission = "student";
     var zoom_pdf = 1;
@@ -24,15 +26,14 @@ export default function (io) {
     //console.log("A user connected");
     socket.on("get-room-info", (roomInfo) => {
       room = roomInfo.roomId;
+      username = roomInfo.username;
       const type = roomInfo.type;
       if (!socket.rooms.has(room)) socket.join(room);
       // const roomPdf = map.forEach((pdf) => pdf.room === room);
       let roomPdf = null;
-      for(let pdf of map)
-        if (pdf.room === room)
-          roomPdf = pdf;
-      for(let i = 0; i<map.length; i++)
-        if(map[i].room === room){
+      for (let pdf of map) if (pdf.room === room) roomPdf = pdf;
+      for (let i = 0; i < map.length; i++)
+        if (map[i].room === room) {
           map_index = i;
           break;
         }
@@ -42,8 +43,8 @@ export default function (io) {
         pdfInfo = roomPdf.pdfInfo;
         zoom_pdf = roomPdf.zoom_pdf;
         scroll_position = roomPdf.scroll_position;
-        current_student_permission = roomPdf.current_student_permission
-        linkGgMeet = roomPdf.linkGgMeet
+        current_student_permission = roomPdf.current_student_permission;
+        linkGgMeet = roomPdf.linkGgMeet;
 
         // io.to(room).emit("get-pdf-status", roomPdf.pdfInfo);
         // io.to(room).emit("set-role", {role: roomPdf.current_student_permission});
@@ -52,30 +53,26 @@ export default function (io) {
         // if(type === "call")
         //   io.to(room).emit("redirect-meeting", {linkMeeting: roomPdf.linkGgMeet})
         socket.emit("get-pdf-status", roomPdf.pdfInfo);
-        socket.emit("set-role", {role: roomPdf.current_student_permission});
+        socket.emit("set-role", { role: roomPdf.current_student_permission });
         socket.emit("pdf-current-zoom", { value: roomPdf.zoom_pdf });
         socket.emit("sync-scrolling-pdf-first-access", roomPdf.scroll_position);
-        if(type === "call")
-          socket.emit("redirect-meeting", {linkMeeting: roomPdf.linkGgMeet})
-
-      }
-      else{
+        if (type === "call")
+          socket.emit("redirect-meeting", { linkMeeting: roomPdf.linkGgMeet });
+      } else {
         var user = {};
         user.pdfInfo = pdfInfo;
         user.zoom_pdf = zoom_pdf;
         user.scroll_position = scroll_position;
         user.room = room;
-        user.current_student_permission = current_student_permission
-        user.linkGgMeet = linkGgMeet
-  
-       // map = map.filter((pdf) => pdf.room !== room);
-       console.log(user)
+        user.current_student_permission = current_student_permission;
+        user.linkGgMeet = linkGgMeet;
+
+        // map = map.filter((pdf) => pdf.room !== room);
+        console.log(user);
 
         map.push(user);
-        map_index = map.length -1;
+        map_index = map.length - 1;
 
-        console.log(map)
-        console.log(map_index)
       }
     });
 
@@ -118,7 +115,8 @@ export default function (io) {
     socket.on("allowance", (role) => {
       current_student_permission = role.role;
       map.forEach((pdf) => {
-        if (pdf.room === room) pdf.current_student_permission = current_student_permission;
+        if (pdf.room === room)
+          pdf.current_student_permission = current_student_permission;
       });
       socket.broadcast.to(room).emit("set-role", role);
     });
@@ -157,23 +155,60 @@ export default function (io) {
 
     socket.on("create-redirect-meeting", (link) => {
       linkGgMeet = link.linkMeeting;
-      console.log(map[map_index])
+      console.log(map[map_index]);
       map[map_index].linkGgMeet = link.linkMeeting;
-      setTimeout(()=>map[map_index].linkGgMeet=null, 1800000);
-      socket.broadcast.to(room).emit('redirect-meeting', {linkMeeting: linkGgMeet})
-    })
+      setTimeout(() => (map[map_index].linkGgMeet = null), 1800000);
+      socket.broadcast
+        .to(room)
+        .emit("redirect-meeting", { linkMeeting: linkGgMeet });
+    });
 
-    socket.on("remove-redirect-meeting",(data)=>{
+    socket.on("remove-redirect-meeting", (data) => {
       console.log("cancel");
-      console.log(map[map_index])
+      console.log(map[map_index]);
 
-      linkGgMeet = null
+      linkGgMeet = null;
       map[map_index].linkGgMeet = null;
 
-      socket.broadcast.to(room).emit('cancel-redirect-meeting', {linkMeeting: linkGgMeet})
-    })
+      socket.broadcast
+        .to(room)
+        .emit("cancel-redirect-meeting", { linkMeeting: linkGgMeet });
+    });
 
-    socket.on("disconnect", () => {
+    socket.on("disconnect", async () => {
+      await axios
+        .post("https://chat.virtedy.com/api/v1/login", {
+          // username: process.env.USER_ADMIN,
+          // password: process.env.PASSWORD_ADMIN,
+          username: "nghianguyen",
+          password: "12345678"
+        })
+        .then(async function (responseMain) {
+         
+          if (responseMain.data.status === "success") {
+            const authTokenAdmin = responseMain.data.data.authToken;
+            const userIdAdmin = responseMain.data.data.userId;
+            await axios.post(
+              `https://chat.virtedy.com/api/v1/chat.postMessage`,
+              {
+                channel: room,
+                alias: " ",
+                emoji: ":none:",
+                text: `**${username}** offline`,
+              },
+              {
+                headers: {
+                  "X-Auth-Token": authTokenAdmin,
+                  "X-User-Id": userIdAdmin,
+                },
+              }
+            );
+          }
+        })
+        .catch(function () {
+          console.log("AUTH FAIL");
+          //res.sendStatus(401);
+        });
       console.log("A user disconnected");
     });
   });
